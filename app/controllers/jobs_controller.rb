df38f9205515c0c1b1e095e740063fb21eb20eaa
India Amos <india@indiamos.com>
2018-03-24 12:07:11 -0400

    Hook up the /jobs/new form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,26 @@
 class JobsController < ApplicationController
   def index
   end
+
+  def new
+    @job = Job.new
+  end
+
+# You _could_ just do
+#   def create
+#     Job.create(params)
+#     redirect_to jobs_path
+#   end
+# and omit the `job_params`, but the method we're using—strong params—limits possible inputs, for added security.
+
+  def create
+    Job.create(job_params)
+    redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
 end
